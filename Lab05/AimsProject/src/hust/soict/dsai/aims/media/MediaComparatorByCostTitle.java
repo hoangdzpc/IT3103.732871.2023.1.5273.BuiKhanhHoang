64e21ab065bf737c75b3eb07d56882e9f1d45a76
hoangdzpc <102605150+hoangdzpc@users.noreply.github.com>
2023-12-29 17:50:45 +0700

    lab05

---
+++ Lab05/AimsProject/src/hust/soict/dsai/aims/media/MediaComparatorByCostTitle.java
@@ -0,0 +1,15 @@
+//Bui Khanh Hoang 20215273
+package hust.soict.dsai.aims.media;
+
+import java.util.Comparator;
+
+public class MediaComparatorByCostTitle implements Comparator<Media>{
+	public int compare(Media media1, Media media2) {
+		int costComparison = Float.compare(media2.getCost(), media1.getCost());
+		if (costComparison != 0) {
+			return costComparison;
+		}
+// 	Nếu giá giống nhau, sắp xếp theo tiêu đề
+		return media1.getTitle().compareTo(media2.getTitle());
+	}
+}
